748eb1eb41cc08adb22f3e7c46efd6f4f127e7d7
KOfW <108273260+KOfW@users.noreply.github.com>
2025-03-28 11:02:22 +0700

    .

--- src/main/java/com/example/assessment_employees/controller/HistoryAssessmentEmployeeController.java
+++ src/main/java/com/example/assessment_employees/controller/HistoryAssessmentEmployeeController.java
@@ -4,6 +4,8 @@ import com.example.assessment_employees.dto.response.HistoryAssessmentEmployeeRe
 import com.example.assessment_employees.service.AssessmentResultService;
 import com.example.assessment_employees.service.HistoryAssessmentEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/assessmentsHistory")
+@RequestMapping("api/assessmentsHistory")
 public class HistoryAssessmentEmployeeController {
     @Autowired
     private HistoryAssessmentEmployeeService historyAssessmentEmployeeService;
--- src/main/java/com/example/assessment_employees/dto/response/HistoryAssessmentEmployeeResponse.java
+++ src/main/java/com/example/assessment_employees/dto/response/HistoryAssessmentEmployeeResponse.java
@@ -7,36 +7,33 @@ import java.math.BigDecimal;
 @Data
 public class HistoryAssessmentEmployeeResponse {
     private Integer id;
-    private String name;
-    private Integer departmentId;
-    private String jobTitle;
-    private BigDecimal score;
-    private Integer assessmentId;
-    private Integer performanceRating;
-    private String feedback;
-    private String assessmentDate;
-    private String comments;
+    private String fullName;
+    private Integer resultId;
+    private String assessmentperiod;
+    private BigDecimal totalScore;
+    private Integer detailId;
+    private Integer criteriaScore;
+    private String comment;
+    private String criteriaName;
+    private String categoryName;
 
     public HistoryAssessmentEmployeeResponse() {}
 
     // Constructor khớp với danh sách tham số
-    public HistoryAssessmentEmployeeResponse(Integer id, String name, Integer departmentId, String jobTitle,
-                                             BigDecimal score, Integer assessmentId, Integer performanceRating,
-                                             String feedback, String assessmentDate, String comments){
 
+
+    public HistoryAssessmentEmployeeResponse(Integer id, String fullName, Integer resultId, String assessmentperiod, BigDecimal totalScore, Integer detailId, Integer criteriaScore, String comment, String criteriaName, String categoryName) {
         this.id = id;
-        this.name = name;
-        this.departmentId = departmentId;
-        this.jobTitle = jobTitle;
-        this.score = score;
-        this.assessmentId = assessmentId;
-        this.performanceRating = performanceRating;
-        this.feedback = feedback;
-        this.assessmentDate = assessmentDate;
-        this.comments = comments;
+        this.fullName = fullName;
+        this.resultId = resultId;
+        this.assessmentperiod = assessmentperiod;
+        this.totalScore = totalScore;
+        this.detailId = detailId;
+        this.criteriaScore = criteriaScore;
+        this.comment = comment;
+        this.criteriaName = criteriaName;
+        this.categoryName = categoryName;
     }
-
-
 }
 
 
